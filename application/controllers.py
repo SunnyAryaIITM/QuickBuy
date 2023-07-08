78fcb229ffbfa85c5fd0fa983284c37d3d23c42d
Sunni Kumar <135229069+SunnyAryaIITM@users.noreply.github.com>
2023-07-08 23:18:38 +0530

    some workk

--- application/controllers.py
+++ application/controllers.py
@@ -5,3 +5,4 @@ App = Blueprint('main', __name__)
 @App.route('/')
 def index_page():
     return render_template("index.html")
+
--- application/models.py
+++ application/models.py
@@ -5,3 +5,4 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String, unique=True)
     email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
